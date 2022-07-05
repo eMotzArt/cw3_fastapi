5a5605d0d69b0661d6bc2b1df465b9ae6346265d
emotzart <johnshuabratsk@gmail.com>
2022-07-06 02:58:49 +0700

    запуск uvicorn'a в app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Request, Response
+import uvicorn
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from project.classes import UserIDentifier
 from project.routes import main_router
 from project.paths import STATIC_PATH
 
@@ -8,3 +8,5 @@ app = FastAPI()
 app.include_router(main_router)
 app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
 
+if __name__ == '__main__':
+    uvicorn.run(app)
